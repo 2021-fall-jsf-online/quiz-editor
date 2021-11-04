cadd34a1e5abb074fe7145c3fee8dd609287a9b3
pnorby <paulnorby@me.com>
2021-11-04 15:17:16 -0500

    showing the dummy quizzes

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -8,23 +8,24 @@ export class QuizService {
   constructor() { }
 
   loadQuizzes = () => {
+
     const quizzesFromWeb: any[] = [
       {
-        name:'Quiz 1',
-        questions:[
+        name: 'Quiz 1'
+        , questions: [
           {
-            name:'Question 1'
-          },
-          {
-            name:'Question 2'
+            name: 'Question 1'
+          }
+          , {
+            name: 'Question 2'
           }
         ]
-      },
-      {
-        name:'Quiz 2',
-        qustions:[]
       }
-    ]
+      , {
+        name: 'Quiz 2'
+        , questions: []
+      }
+    ];
 
     return quizzesFromWeb;
   };
