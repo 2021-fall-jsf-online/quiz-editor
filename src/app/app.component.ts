d7aa1b423e7e893c934684775d82a2ccd19cb78d
Jeri <79335135+jeriaca@users.noreply.github.com>
2021-11-28 11:22:17 -0600

    newly added quiz set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ interface QuizDisplay {
   quizName: string;
   quizQuestions: QuestionDisplay[];
   markedForDelete: boolean;
+  newlyAddedQuiz: boolean;
 }
 
 interface QuestionDisplay {
@@ -41,6 +42,7 @@ export class AppComponent implements OnInit {
           questionName: y.name
         }))
         , markedForDelete: false
+        , newlyAddedQuiz: false
       }));      
 
       this.loading = false;
@@ -71,6 +73,7 @@ export class AppComponent implements OnInit {
       quizName: "Untitled Quiz"
       , quizQuestions: []
       , markedForDelete: false
+      , newlyAddedQuiz: true
     };
 
     this.quizzes = [
@@ -165,5 +168,5 @@ export class AppComponent implements OnInit {
 
   get deletedQuizCount() {
     return this.getDeletedQuizzes().length;
-  }
+  };
 }
