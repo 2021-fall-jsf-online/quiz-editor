1b1a2a9d8bfa3791fcbba5cc1754524fec680757
Matt TerBeek <matthewevansterbeek@gmail.com>
2021-11-30 20:47:17 -0600

    Edited quizzes working - newQuizzes....not so much

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { QuizService, QuizFromWeb } from './quiz.service';
+import { QuizService, QuizFromWeb, ShapeForSavingEditedQuizzes, ShapeForSavingNewQuizzes } from './quiz.service';
 import {
   trigger,
   transition,
@@ -232,4 +232,35 @@ export class AppComponent implements OnInit {
   detailsFromLeftAnimationDone = () => {
     this.detailsFromLeftAnimationState = "leftPosition";
   };
+
+  saveQuizzes = async () => {
+    try {
+      
+      const newQuizzes: ShapeForSavingNewQuizzes[] = this.getAddedQuizzes().map(x => ({
+        quizName: x.quizName,
+        quizQuestions: []
+        // quizQuestions: x.quizQuestions[]
+        //   question: y.questionName
+        // }))
+      }));
+      const editedQuizzes: ShapeForSavingEditedQuizzes[] = this.getEditedQuizzes().map(x => ({
+        quiz: x.quizName,
+        questions: x.quizQuestions.map(y => ({
+        question: y.questionName
+        }))
+      }));
+
+      const numberOfUpdatedQuizzes = await this.quizSvc.saveQuizzes(
+        editedQuizzes,
+        newQuizzes
+      );
+
+      console.log("numberOfUpdatedQuizzes", numberOfUpdatedQuizzes);
+    }
+    
+    catch (err) {
+      console.error(err);
+    }
+  }
+
 }
