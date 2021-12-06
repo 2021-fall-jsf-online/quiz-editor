7d003eccc0ee80ef8b3ad7fcb809b46350fb1918
g-batista <gbatista@madisoncollege.edu>
2021-12-06 13:22:12 -0600

    Add the loading message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,11 +24,14 @@ export class AppComponent implements OnInit {
   ) {
   }
 
+  loading = false;
   errorLoadingQuizzes = false;
 
   loadQuizzesFromCloud = async () => {
 
     try {
+      this.loading = true;
+
       const quizzes = await this.quizSvc.loadQuizzes();
       console.log(quizzes);
 
@@ -38,11 +41,14 @@ export class AppComponent implements OnInit {
           questionName: y.name
         }))
         , markedForDelete: false
-      }));      
+      }));
+      
+      this.loading=false;
     }
     catch (err) {
       console.error(err);
-      this.errorLoadingQuizzes = true;      
+      this.errorLoadingQuizzes = true; 
+      this.loading = false;    
     }
   };
 
