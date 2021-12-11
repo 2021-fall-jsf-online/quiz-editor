d471c1718615cb00228e21ddd55dc04b87d415f1
g-batista <gbatista@madisoncollege.edu>
2021-12-11 17:26:02 -0600

    Got details from  left animation working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http/';
 
-import{BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import{ BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
